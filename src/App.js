f039dd23baa24fa31b1533ce85a5f432aef251bb
ottogutierrez <ottogutierrez@me.com>
2021-01-13 23:32:15 -0500

    Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import NavBar from './components/NavBar'
-
+import HomePage from './components/HomePage'
 function App() {
   return (
-    <div className="App">
-      <NavBar />
+    <div className="App ">
+    
     </div>
   );
 }
---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+
+  return(
+    <div className="text-6xl font-bold p-24 w-full text-center">
+      <p>Just organize</p>
+      <p>your tasks man...</p>
+    </div>
+  )
+}
+
+export default HomePage
